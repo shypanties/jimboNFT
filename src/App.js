b46b0afe0a477de097c9c9f5e6cc5cee6ed2d979
Keeveee <shypanties22@gmail.com>
2022-10-27 15:57:00 +0800

    .

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import { useState } from 'react';
 import { ethers, BigNumber } from 'ethers';
 import jimboNFT from './JimboNFT.json';
 import './App.css';
-import { Box, Button, Flex, Image, Link, Spacer, Input, Text } from '@chakra-ui/react';
+import shotcallers from "./assets/1.jpg";
+import {Image} from '@chakra-ui/react';
 
 // Assets
-import Twitter from "./assets/social-media-icons/twitter_32x32.png";
-import Discord from "./assets/social-media-icons/discord_32x32.png";
-import Linkedin from "./assets/social-media-icons/linkedIn_32x32.png";
-import Opensea from "./assets/social-media-icons/opensea_32x32.png";
 import Jimbo from "./assets/mascot.gif";
 
 function App() {
@@ -56,138 +53,87 @@ function App() {
   };
   
   return (
-    <div className="overlay">
-      <div className="App">
-        {/* Left Side */}
-        <Flex justify="space-between" align="center" padding="40px">
-          <Flex justify="space-around" width="auto" padding="0 25px">
-            <Link href="https://www.opensea.io">
-              <Image height="5vh" src={Opensea}/>
-            </Link>
-          </Flex>
-          <Flex justify="space-around" width="auto" padding="0 25px">
-            <Link href="https://discord.gg/psg3qjfq">
-              <Image height="5vh" src={Discord}/>
-            </Link>
-          </Flex>
-          <Flex justify="space-around" width="auto" padding="0 25px">
-            <Link href="https://www.twitter.com">
-              <Image height="5vh" src={Twitter}/>
-            </Link>
-          </Flex>
-          <Flex justify="space-around" width="auto" padding="0 25px">
-            <Link href="https://www.linkedin.com/">
-              <Image height="5vh" src={Linkedin}/>
-            </Link>
-          </Flex>
-        
-        {/*Right Side */}
-          <Spacer />
-          {isConnected ? (
-            <Box margin="0 15px">{connectedWallet}</Box>
-          ) : (
-            <Button 
-              backgroundColor="#D6517D"
-              borderRadius="10px"
-              boxShadow="0px 2px 2px 1px #0F0F0F"
-              color="white"
-              cursor="pointer"
-              fontFamily="inherit"
-              padding="15px 25px"
-              margin="0 15px"
-              onClick={connectAccount}>
-              
-              Connect
-            </Button>
-          )}
-        </Flex> 
-
-        {/* Main Mint */}
-        <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
-          <Box width="520px">
-            <div>
-              <Image 
-                align="center" 
-                justify="center" 
-                height = "50vh" 
-                paddingBottom= "20px" 
-                src={Jimbo}
-              />
-              <p>JIMBO : Just Jimbo</p>
-            </div>
-            {isConnected ? (
-              <div>
-                <Flex align="center" justify="center">
-                  <Button 
-                    backgroundColor="#D6517D"
-                    borderRadius="5px"
-                    boxShadow="0px 2px 2px 1px #0F0F0F"
-                    color="white"
-                    cursor="pointer"
-                    fontFamily="inherit"
-                    padding="15px"
-                    marginTop="10px"
-                    onClick={() => handleDecrement()}
-                  >
-                    -
-                  </Button>
-                  <Input
-                    readOnly
-                    fontFamily="inherit"             
-                    width="100px"
-                    height="40px"
-                    textAlign={'center'}
-                    paddingLeft="19x"
-                    marginTop="10px"
-                    type="number"
-                    value={mintAmount}
-                  />
-                  <Button 
-                    backgroundColor="#D6517D"
-                    borderRadius="5px"
-                    boxShadow="0px 2px 2px 1px #0F0F0F"
-                    color="white"
-                    cursor="pointer"
-                    fontFamily="inherit"
-                    padding="15px"
-                    marginTop="10px"
-                    onClick={() => handleIncrement()}
-                  >
-                    +
-                  </Button>
-                </Flex>
-                <div>
-                  <Button 
-                    backgroundColor="#D6517D"
-                    borderRadius="5px"
-                    boxShadow="0px 2px 2px 1px #0F0F0F"
-                    color="white"
-                    cursor="pointer"
-                    fontFamily="inherit"
-                    padding="15px"
-                    marginTop="10px"
-                    onClick={handleMint} 
-                  >
-                    MINT NOW
-                  </Button>
-                </div>
-              </div>
-            ) : (
-              <Text
-                marginTop="20px"
-                fontSize="25px"
-                letterSpacing="-5.5%"
-                fontFamily="VT323"
-                textShadow="0 3px #000000"
-                color="#D6517D"
-              > 
-                You must be connected to Mint. 
-              </Text>
-            )}
-          </Box>
-        </Flex>
+    <div class="App">
+        <div class="scanline"></div>
+      <div class="content clearfix">
+      <div justify="space-between" align="left" padding="40px"> 
+      <a2>Twitter  </a2>
+      <a2>  Discord</a2>
       </div>
-      <div className="moving-background"></div>
+      <div justify="space-between" align="right" padding="40px">        
+      {/*Right Side */}
+        {isConnected ? (
+          <div margin="0 15px">{connectedWallet}</div>
+        ) : (
+          <a3 onClick={connectAccount}> Connect Wallet </a3>
+        )}
+      </div> 
+      <br></br>
+      <div></div>
+
+      {/* Main Mint */}
+      <div justify="center" align="center" height="100vh" paddingBottom="150px">
+        <div>
+          <div>
+            <div>
+              <Image width = "120vh" src={shotcallers} max-width = "80%"></Image>
+            </div>
+          </div>
+          {isConnected ? (
+            <div>
+              <div>
+                <a 
+                  onClick={() => handleDecrement()}
+                >
+                  -
+                </a>
+                <textarea readOnly value={mintAmount}/>
+                <a
+                  onClick={() => handleIncrement()}
+                >
+                  +
+                </a>
+              </div>
+              <p>0.07 ETH | Max Mint: 5 per wallet</p>
+              <div>
+                <a
+                  backgroundColor="#D6517D"
+                  borderRadius="5px"
+                  boxShadow="0px 2px 2px 1px #0F0F0F"
+                  color="white"
+                  cursor="pointer"
+                  fontFamily="inherit"
+                  padding="15px"
+                  marginTop="10px"
+                  onClick={handleMint} 
+                >
+                  MINT NOW
+                </a>
+              </div>
+            </div>
+          ) : (
+            <text
+              marginTop="20px"
+              fontSize="25px"
+              letterSpacing="-5.5%"
+              fontFamily="VT323"
+              textShadow="0 3px #000000"
+              color="#D6517D"
+            > 
+              You must be connected to Mint. 
+            </text>
+          )}
+        </div>
+      </div>
+      </div>
+      <p><br></br><br></br><br></br><br></br><br></br><br></br></p>
+      <p><br></br><br></br><br></br><br></br><br></br><br></br></p>
+      <p><br></br><br></br><br></br><br></br><br></br><br></br></p>
+      <p><br></br><br></br><br></br><br></br><br></br><br></br></p>
+      <p><br></br><br></br><br></br><br></br><br></br><br></br></p>
+      <p><br></br><br></br><br></br><br></br><br></br><br></br></p>
+      <p><br></br><br></br><br></br><br></br><br></br><br></br></p>
+      <div class="overlay"></div>
     </div>
   );
 }
